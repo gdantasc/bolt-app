a5c89e659323a8cb4d94e767e73bb14c12598e20
Leonardo Oliveira Balsalobre <leonardo.balsalobre.ext@infracommerce.com.br>
2021-10-22 11:30:01 -0300

    feat: add filter events

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -3,13 +3,14 @@
     <v-app>
       <v-main>
         <div class="search">
-          <Input @input.native="search($event.target.value)" />
+          <Input @input.native="filter = $event.target.value" />
         </div>
-        <div v-if="filteredItems.length">
-          <Card v-for="event in filteredItems" :key="event.id" :event="event" />
-        </div>
-        <div v-else>
-          <Card v-for="event in availableEvents" :key="event.id" :event="event" />
+        <div>
+          <Card
+            v-for="event in filteredItems"
+            :key="event.id"
+            :event="event"
+          />
         </div>
       </v-main>
     </v-app>
@@ -28,7 +29,7 @@ export default {
   },
   data: () => ({
     events: [],
-    filteredItems: [],
+    filter: ''
   }),
   async mounted() {
     await this.loadEvents();
@@ -38,18 +39,22 @@ export default {
       const response = await axios.get("http://localhost:3030/event");
       this.events = response.data;
     },
-    search(eventName) {
-      const events = this.events.filter((event) => {
-        return event.name.toLowerCase().includes(eventName.toLowerCase());
-      });
-      this.filteredItems = events;
-    },
   },
   computed: {
     availableEvents() {
       return this.events
         .filter((event) => event.available_vacancies > 0)
         .sort((a, b) => (a.name > b.name ? 1 : -1));
+    },
+    filteredItems() {
+      console.log(this.filter)
+      if (this.filter) {
+        const events = this.events.filter((event) => {
+          return event.name.toLowerCase().includes(this.filter.toLowerCase());
+        });
+        return events;
+      }
+      return this.availableEvents;
     },
   },
 };
